0a669c1fc8888f1f1efd3fc9a616bf2d9fee0c1e
Axamit <axamitta@gmail.com>
2025-08-11 09:08:23 -0400

    Mapea datos de los libros y refactoriza la clase Principal con métodos muestraElMenu y buscarLibro

--- src/main/java/com/aluracursos/literalura/principal/Principal.java
+++ src/main/java/com/aluracursos/literalura/principal/Principal.java
@@ -1,19 +1,46 @@
 package com.aluracursos.literalura.principal;
 
+import com.aluracursos.literalura.model.DatosLibro;
+import com.aluracursos.literalura.model.RespuestaLibros;
 import com.aluracursos.literalura.service.ConsumoAPI;
 import com.aluracursos.literalura.service.ConvierteDatos;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
 public class Principal {
 
-    private ConsumoAPI consumoApi = new ConsumoAPI();
     private final String URL_BASE = "https://gutendex.com/books/";
+    private Scanner teclado = new Scanner(System.in);
+    private ConsumoAPI consumoApi = new ConsumoAPI();
     private ConvierteDatos conversor = new ConvierteDatos();
 
-    public void ejecutar() {
-        String json = consumoApi.obtenerDatos(URL_BASE);
-        // Aquí conviertes el json a los objetos que hayas definido, por ejemplo:
-        // Libro[] libros = conversor.obtenerDatos(json, Libro[].class);
+    public void muestraElMenu() {
+        System.out.println("Por favor, escribe el nombre del libro que deseas buscar");
+        var tituloDelLibro = teclado.nextLine();
+
+        String tituloEncoded = URLEncoder.encode(tituloDelLibro, StandardCharsets.UTF_8);
+        var json = consumoApi.obtenerDatos(URL_BASE + "?search=" + tituloEncoded);
+
         System.out.println("Respuesta JSON de la API:");
-        System.out.println(json); // Para probar que trae la info
+        System.out.println(json);
+        buscarLibro(json);
+
+
+    }
+
+    public void buscarLibro(String json) {
+        var respuesta = conversor.obtenerDatos(json, RespuestaLibros.class);
+
+        if (respuesta.resultados() != null && !respuesta.resultados().isEmpty()) {
+            var libro = respuesta.resultados().get(0);
+            System.out.println("Primer libro encontrado: " + libro.titulo());
+            System.out.println("Autor principal: " + libro.autorPrincipal().nombre());
+            System.out.println("Idioma principal: " + libro.idiomaPrincipal());
+            System.out.println("Número de descargas: " + libro.numeroDescargas());
+        } else {
+            System.out.println("No se encontraron libros para esa búsqueda.");
+        }
     }
 }
